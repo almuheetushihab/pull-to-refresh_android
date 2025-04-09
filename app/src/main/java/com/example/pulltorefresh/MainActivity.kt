543c1ae5026ad9bed7ef4fb36f1c9b796cd405e0
Almuheetu shihab <almuheetu@gmail.com>
2025-04-09 11:32:51 +0600

    feat: pull to refresh implementation

--- app/src/main/java/com/example/pulltorefresh/MainActivity.kt
+++ app/src/main/java/com/example/pulltorefresh/MainActivity.kt
@@ -6,42 +6,73 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pulltorefresh.ui.theme.PullToRefreshTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PullToRefreshTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val refreshState = rememberPullToRefreshState()
+                var isRefreshing by remember { mutableStateOf(false) }
+
+                val items = remember {
+                    (0..40).toList()
+                }
+
+                val coroutineScope = rememberCoroutineScope()
+
+                PullToRefreshBox(
+                    state = refreshState,
+                    isRefreshing = isRefreshing,
+                    onRefresh = {
+                        coroutineScope.launch {
+                            isRefreshing = true
+                            delay(5.seconds)
+                            isRefreshing = false
+                        }
+                    }
+
+                ) {
+                    LazyColumn {
+                        items(items) {
+                            ListItem(
+                                headlineContent = {
+                                    Text(text = "Item $it")
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PullToRefreshTheme {
-        Greeting("Android")
     }
 }
